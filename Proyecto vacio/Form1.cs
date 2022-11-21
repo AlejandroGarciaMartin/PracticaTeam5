3bfcce17a761e4e931cd075866eb265c88a65fe8
gabris <gabris@AULA2452>
2022-11-21 18:45:51 +0100

    Errores arreglados

--- Proyecto vacio/Form1.cs
+++ Proyecto vacio/Form1.cs
@@ -26,22 +26,24 @@ namespace Proyecto_vacio
         {
             {
                 string textoTelegrama;
-                char tipoTelegrama = ' ';
+                char tipoTelegrama = 'o';
                 int numPalabras = 0;
                 double coste;
                 //Leo el telegrama
                 textoTelegrama = txtTelegrama.Text;
                 // telegrama urgente?
+
                 if (cbUrgente.Checked)
                     tipoTelegrama = 'u';
+
                 //Obtengo el número de palabras que forma el telegrama
-                numPalabras = textoTelegrama.Length;
+                numPalabras = textoTelegrama.Split().Length;
                 //Si el telegrama es ordinario
                 if (tipoTelegrama == 'o')
                     if (numPalabras <= 10)
-                        coste = 25;
+                        coste = 2.5;
                     else
-                        coste = 0.5 * numPalabras;
+                        coste = 2.5 + .5 * (numPalabras - 10);
                 else
                 //Si el telegrama es urgente
                 if (tipoTelegrama == 'u')
